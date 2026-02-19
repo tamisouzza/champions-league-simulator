aa9f83aedbbe7e1f3b0eaca9dbc436523316bcb1
tamisouzza <tamirissoares91@gmail.com>
2026-02-18 23:12:11 -0300

    Configure routing, SSR and teams page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Teams } from './pages/teams/teams';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'teams', pathMatch: 'full' },
+  { path: 'teams', component: Teams },
+];
